7b9078c4787c37c65639b15ed0531e4b374838ef
KabirSamsi <35539143+KabirSamsi@users.noreply.github.com>
2018-12-02 11:50:03 -0800

    Added comments and changed grid array

--- clickit.js
+++ clickit.js
@@ -1,3 +1,4 @@
+// DOM elements
 const container = document.querySelector('.container')
 const buttons = container.querySelector('.buttons')
 const scores = document.querySelector('.scores')
@@ -7,10 +8,14 @@ const h1 = document.querySelector('h1')
 const total = document.querySelector('#clicked')
 const red = document.querySelector('#red_clicked')
 const grey = document.querySelector('#grey_clicked')
+
+//Canvas elements
 const canvas = document.querySelector('canvas')
 const width = canvas.width = window.innerWidth
 const height = canvas.height = window.innerHeight
 let ctx = canvas.getContext('2d')
+
+//Variable game elements
 let randomX;
 let randomY;
 let new_total = 0
@@ -18,7 +23,9 @@ let new_red = 0
 let new_grey = 0
 let changed = 0
 let color;
+let squares = []
 
+//Creating start button
 const start = document.createElement('button')
 start.innerHTML = 'Start Game'
 start.style.width = '150px'
@@ -27,7 +34,9 @@ start.style.backgroundColor = 'darkblue'
 start.style.color = 'white'
 buttons.insertBefore(start, span)
 
+//Game begins on start click
 start.addEventListener('click', () => {
+  //Creates quit button
   const quit = document.createElement('button')
   quit.innerHTML = 'Quit Game'
   quit.style.backgroundColor = 'turquoise'
@@ -37,11 +46,13 @@ start.addEventListener('click', () => {
   buttons.insertBefore(quit, br)
   let timeout = parseFloat(document.querySelector('#timeout').value)*1000
 
+  //Function to remove square
   const kill = (x, y) => {
     ctx.fillStyle = 'grey'
     ctx.fillRect(x, y, 30, 30)
   }
 
+  //Function to create square
   const generate_random_coordinates = () => {
     randomX = Math.round((Math.random()*width)/32)*32
     randomY = Math.round((Math.random()*(height-170))/32)*32
@@ -51,6 +62,7 @@ start.addEventListener('click', () => {
 
   }
 
+  //Js class to structure square
   class Tile {
     constructor(x, y, color, number) {
       this.x = x;
@@ -65,8 +77,7 @@ start.addEventListener('click', () => {
 
   }
 
-  squares = []
-
+  //Creates squares grid
   for (let y = 0; y < height-170; y += 32) {
     for (let i = 0; i < width; i += 32) {
       let square = new Tile(i, y, 'grey')
@@ -75,19 +86,22 @@ start.addEventListener('click', () => {
     }
   }
 
+  //Creates red flashing square
   changing = window.setInterval(generate_random_coordinates, timeout)
 
+  //Allows user to click red grid
   canvas.addEventListener('click', e => {
     if (changed === 0) {
       new_total += 1
       total.innerHTML = `Total Squares Clicked: ${new_total}`
       for (let squareset of squares) {
+        //If user clicks red square
         if (((Math.round(e.pageX/32)*32) == randomX || ((Math.round(e.pageX/32)*32)-32 == randomX) || ((Math.round(e.pageX/32)*32)+32 == randomX))) {
         new_red += 1
         red.innerHTML = `Red Squares Clicked: ${new_red}`
         break;
 
-
+        //If user clicks grey square
         } else {
           new_grey += 1
           grey.innerHTML = `Grey Squares Clicked: ${new_grey}`
@@ -97,9 +111,11 @@ start.addEventListener('click', () => {
     }
   })
 
+  //Exits game if user clicks quit button
   quit.addEventListener('click', () => {
     clearInterval(changing)
     changed = 1
+    //Creates and displays score
     const score = document.createElement('strong')
     if (new_total === 0) {
       score.innerHTML = `<br><br>Score: 0/0 (0%)`
@@ -108,8 +124,7 @@ start.addEventListener('click', () => {
       score.innerHTML = `<br><br>Score: ${new_red} / ${new_total} (${(new_red/new_total)*100}%)`
 
     }
-
     scores.appendChild(score)
-
+    
   })
 })

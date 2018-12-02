03d7e4873ffc24f570836e1c0a7d9be8384fb21e
KabirSamsi <35539143+KabirSamsi@users.noreply.github.com>
2018-12-02 00:58:55 -0800

    Desperate Attempt

--- clickit.js
+++ clickit.js
@@ -81,18 +81,19 @@ start.addEventListener('click', () => {
   })
 
   start.addEventListener('click', () => {
-    clearInterval(changing)
-    changed = 1
-    const score = document.createElement('strong')
-    if (new_total === 0) {
-      score.innerHTML = `<br><br>Score: 0/0 (0%)`
+    if (start.innerHTML === 'Quit Game') {
+      clearInterval(changing)
+      changed = 1
+      const score = document.createElement('strong')
+      if (new_total === 0) {
+        score.innerHTML = `<br><br>Score: 0/0 (0%)`
 
-    } else {
-      score.innerHTML = `<br><br>Score: ${new_red} / ${new_total} (${(new_red/new_total)*100}%)`
+      } else {
+        score.innerHTML = `<br><br>Score: ${new_red} / ${new_total} (${(new_red/new_total)*100}%)`
 
+      }
+
+      container.appendChild(score)
     }
-
-    container.appendChild(score)
-
   })
 })
